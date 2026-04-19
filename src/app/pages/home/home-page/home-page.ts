1db0b7b127d38f6ba78ef8185e3e5c5c8c50044b
Peterson Fontinhas <petersonfontinhas@gmail.com>
2026-04-18 23:44:30 -0300

    home-banner adicionado

--- src/app/pages/home/home-page/home-page.ts
+++ src/app/pages/home/home-page/home-page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComp } from "../../../shared/components/header-comp/header-comp";
 import { FooterComp } from "../../../shared/components/footer-comp/footer-comp";
+import { HomeBanner } from "../home-banner/home-banner";
 
 @Component({
   selector: 'app-home-page',
-  imports: [HeaderComp, FooterComp],
+  imports: [HeaderComp, FooterComp, HomeBanner],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
